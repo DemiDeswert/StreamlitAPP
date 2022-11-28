757f3ed1b72866213707ae7b9c233326db5ac4f9
Demi Deswert <r0784118@student.thomasmore.be>
2022-11-28 14:38:21 +0100

    nieuwapp

---
+++ Streamlitapp.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pickle
+from PIL import Image
+import io
+
+def load_image():
+    uploaded_file = st.file_uploader(label='Pick an image to test')
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        st.image(image_data)
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+def main():
+    st.title('Pretrained model demo')
+    image = load_image()
+    result = st.button('Run on image')
+    model = pickle.load(open('model.pkl', 'rb'))
+    if result:
+        st.write('Calculating results...')
+        model.predict(image, confidence=40, overlap=30).save("prediction.jpg")
+        prediction = Image.open('prediction.jpg')
+        st.image(prediction)
+
+if __name__ == '__main__':
+    main()
