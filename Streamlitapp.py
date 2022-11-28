6c1dfed98099539f7061264dd83cdffb9aeddee2
Demi Deswert <r0784118@student.thomasmore.be>
2022-11-28 14:57:49 +0100

    Update Streamlitapp.py

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -2,11 +2,13 @@ import streamlit as st
 import pickle
 from PIL import Image
 import io
+import os
 
 def load_image():
     uploaded_file = st.file_uploader(label='Pick an image to test')
     if uploaded_file is not None:
-        return uploaded_file
+        image =os.stat(str(uploaded_file))
+        return image
     else:
         return None
 def main():
@@ -16,9 +18,8 @@ def main():
     model = pickle.load(open('model.pkl', 'rb'))
     if result:
         st.write('Calculating results...')
-        predictionimg=""
-        model.predict(image, confidence=40, overlap=30).save(predictionimg)
-        prediction = Image.open(predictionimg)
+        model.predict(image, confidence=40, overlap=30).save("./prediction/prediction.jpg")
+        prediction = Image.open("./prediction/prediction.jpg")
         st.image(prediction)
 
 if __name__ == '__main__':
